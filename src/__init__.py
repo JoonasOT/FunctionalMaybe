e546bb96d7dd55d9aff11a5447465032afa738f1
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-11-19 03:05:51 +0200

    Fix module path

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["FunctionalMaybe", "Empty", "TestMaybe"]
 
 from src.FunctionalMaybe.functional_maybe import FunctionalMaybe
-from src.FunctionalMaybe import Empty
+from src.FunctionalMaybe.empty import Empty
 from src.tests.test_maybe import TestMaybe
